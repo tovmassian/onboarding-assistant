95527545f98579fe6a2ebeb691d6444a335173cd
Tovmasyan, Khachatur [External] <Khachatur.Tovmasyan@externals.adidas.com>
2024-09-02 11:35:22 +0200

    pass appId and appPassword variables through github repository

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const restify = require('restify');
 const botbuilder = require('botbuilder');
 
 // Create bot adapter, which defines how the bot sends and receives messages.
+console.log(`app id: ${process.env.MicrosoftAppId}`);
 var adapter = new botbuilder.BotFrameworkAdapter({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
@@ -19,11 +20,12 @@ server.post('/api/messages', (req, res, next) => {
     // Use the adapter to process the incoming web request into a TurnContext object.
     adapter.processActivity(req, res, async (turnContext) => {
         // Do something with this incoming activity!
-        if (turnContext.activity.type === 'message') {            
-            // Get the user's text
-            const utterance = turnContext.activity.text;
-            // send a reply
-            await turnContext.sendActivity(`I heard you say ${ utterance }`);
+        if (turnContext.activity.type === 'message') {
+          // Get the user's text
+          const utterance = turnContext.activity.text;
+          console.log(`Received message: ${utterance}`); // Log received message
+          // send a reply
+          await turnContext.sendActivity(`I heard you say ${utterance}`);
         }
     }).catch((err) => {
         console.error('Error processing request', err);
